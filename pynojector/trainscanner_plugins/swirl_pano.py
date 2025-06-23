2f661f64058ed0bce9922fe12290e81dee8d8984
vitroid <vitroid@gmail.com>
2025-06-23 19:13:51 +0900

    swirl_projection関数にfinish引数を追加し、出力画像の形式に応じた処理を実装しました。また、convert関数内でもfinish引数を使用し、出力のトリミング処理を条件分岐で行うように修正しました。コマンドライン引数にfinishオプションを追加し、説明を更新しました。

--- pynojector/trainscanner_plugins/swirl_pano.py
+++ pynojector/trainscanner_plugins/swirl_pano.py
@@ -2,6 +2,8 @@ from pynojector import (
     imagestrip_from_mercator_ribbon,
     conversion,
     mercator_from_equirectangular,
+    mercator_from_stereographic,
+    stereographic_from_peirce_quincuncial,
 )
 import numpy as np
 import cv2
@@ -18,13 +20,22 @@ __all__ = ["get_parser", "make_movie", "movie_iter"]
 def swirl_projection(
     z: np.ndarray,
     tilt_angle: float = 45,
+    finish: str = "pano",
 ) -> np.ndarray:
 
     # 関数名のつけかたに困惑している。
     # 一番左に、原画像の形式があり、一番右に、出力画像の形式がくるんだが、データの流れは逆向きなんだよね。
+    if finish == "pano":
+        z = mercator_from_equirectangular(z=z)
+    elif finish == "swirl":
+        z = mercator_from_stereographic(z=z)
+    elif finish == "square":
+        z = mercator_from_stereographic(z=stereographic_from_peirce_quincuncial(z=z))
+    else:
+        raise ValueError(f"Invalid finish: {finish}")
 
     z = imagestrip_from_mercator_ribbon(
-        z=mercator_from_equirectangular(z=z),
+        z=z,
         theta=np.radians(tilt_angle),
     )
     return z
@@ -34,6 +45,7 @@ def convert(
     img: np.ndarray,
     width: int = 1440,
     tilt_angle: float = 45,
+    finish: str = "pano",
     **kwargs,
 ):
     logger = logging.getLogger(__name__)
@@ -43,23 +55,29 @@ def convert(
     projector = lambda z: swirl_projection(
         z=z,
         tilt_angle=tilt_angle,
+        finish=finish,
     )
     o = conversion(projector=projector, src_image=img, size=width)
-    o = o[width // 4 : width * 3 // 4, :]
+    if finish == "pano":
+        o = o[width // 4 : width * 3 // 4, :]
     return o
 
 
 def get_parser():
-    parser = argparse.ArgumentParser(
-        description="Make a cylindrical panorama image (2:1)"
-    )
+    parser = argparse.ArgumentParser(description="Make a swirl image")
     parser.add_argument("filename", type=str, help="Input image file")
     parser.add_argument("--debug", action="store_true", help="Debug mode")
     parser.add_argument(
-        "--tilt_angle", type=float, default=15, help="Tilt angle--0.5,89"
+        "--tilt_angle", type=float, default=15, help="Tilt angle -- 0.5:89"
     )
     parser.add_argument(
-        "--width", type=int, default=4000, help="Width of the panorama -- 400,20000"
+        "--width", type=int, default=4000, help="Width of the panorama -- 400:20000"
+    )
+    parser.add_argument(
+        "--finish",
+        type=str,
+        default="pano",
+        help="Type of the image -- pano|swirl|square",
     )
     return parser
 
