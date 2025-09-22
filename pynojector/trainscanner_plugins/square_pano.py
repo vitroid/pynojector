d6e4f6ce2b69596780779c69cea3a1a7a4b61d8f
vitroid <vitroid@gmail.com>
2025-09-22 10:51:11 +0900

    Refactor square_pano.py and remove swirl_pano.py. Updated imports and reorganized code structure in square_pano.py for clarity. The swirl_pano.py file has been deleted as its functionality is now integrated into square_pano.py.

--- pynojector/trainscanner_plugins/square_pano.py
+++ pynojector/trainscanner_plugins/square_pano.py
@@ -1,17 +1,16 @@
-from pynojector import (
-    imagestrip_from_mercator_ribbon,
-    conversion,
-    mercator_from_stereographic,
-    stereographic_from_peirce_quincuncial,
-)
-import numpy as np
-import cv2
-import tempfile
-import os
-import subprocess
-import tqdm
 import argparse
 import logging
+import os
+import subprocess
+import tempfile
+
+import cv2
+import numpy as np
+import tqdm
+
+from pynojector import (conversion, imagestrip_from_mercator_ribbon,
+                        mercator_from_stereographic,
+                        stereographic_from_peirce_quincuncial)
 
 __all__ = ["get_parser", "make_movie", "movie_iter"]
 
@@ -20,7 +19,6 @@ def swirl_projection(
     z: np.ndarray,
     tilt_angle: float = 45,
 ) -> np.ndarray:
-
     # 関数名のつけかたに困惑している。
     # 一番左に、原画像の形式があり、一番右に、出力画像の形式がくるんだが、データの流れは逆向きなんだよね。
 
